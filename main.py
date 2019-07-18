af333e9015a2a1a558721afa2aedaf301a10003b
dh <dhfromkorea@gmail.com>
2019-07-17 21:11:24 -0400

    implement basic policies - sample mean - random - eps greedy - ucb

--- main.py
+++ main.py
@@ -37,13 +37,13 @@ def sample_mushroom(X, y,
     contexts = X[indices, :]
     r_no_eats = r_no_eat * np.ones(n_mushrooms)
     r_eats = np.zeros(n_mushrooms)
-    # y_i == 1 => good
-    r_eats += r_eat_good * y[indices]
-    # y_i == 0 ==> bad
+    # y_i == 1 => bad
+    r_eats += r_eat_good * ~y[indices].astype(bool)
+    # y_i == 0 ==> good
     r_eat_bad = np.random.choice([r_eat_bad_lucky, r_eat_bad_unlucky],
                                  p=[r_eat_bad_lucky_prob, 1-r_eat_bad_lucky_prob],
                                  size=n_mushrooms)
-    r_eats += r_eat_bad * ~y[indices].astype(bool)
+    r_eats += r_eat_bad * y[indices]
 
     # E[R_{t,a} | A_t=1, C_t=c_t, P_t=1]
 
@@ -51,7 +51,7 @@ def sample_mushroom(X, y,
     # E[R_t,a | no_eat, good] = E[R_t,a | no_eat, bad]
     # = 0
     # E[R_t,a | eat, bad], E[R_t,a | eat, good]
-    # =-15, 5
+    # = 1, 5
     # assume E[R_t,a | eat, good] > rest
     E_r_no_eat = r_no_eat
     E_r_eat_bad = r_eat_bad_lucky * r_eat_bad_lucky_prob +\
@@ -59,7 +59,7 @@ def sample_mushroom(X, y,
     if E_r_no_eat > E_r_eat_bad:
         # not take risks
         # still eat when good
-        opt_acts = y[indices]
+        opt_acts = (~y[indices].astype(bool)).astype(int)
     else:
         # take risks
         # always eat
@@ -91,38 +91,146 @@ def main(arg1):
     features, X = df_.columns, df_.values
     y = df.iloc[:, 0].values
     label_encoder_y = LabelEncoder()
+    # y = 1 -> poisonous, y = 0 -> edible
     y = label_encoder_y.fit_transform(y)
 
 
     # define a solver
-    policy = random_policy
+    rp = RandomPolicy(n_actions=2)
+    smp = SampleMeanPolicy(n_actions=2, lr=0.1)
+    egp = EpsilonGreedyPolicy(n_actions=2, lr=0.1, epsilon=0.1)
+    ucbp = UCBPolicy(n_actions=2, lr=0.01)
+
+    policies = [rp, smp, egp, ucbp]
+    results = {}
+
 
     # simulate the problem T steps
-    T = 100
-    mushrooms = sample_mushroom(X, y, T)
-    cum_rewards = 0.0
-    cum_regrets = 0.0
-    t = 0
+    T = 100000
+    mushrooms = sample_mushroom(X,
+                                y,
+                                T,
+                                r_eat_good=5.0,
+                                r_eat_bad_lucky=5.0,
+                                r_eat_bad_unlucky=-35.0,
+                                r_eat_bad_lucky_prob=0.5,
+                                r_no_eat=0.0
+                                )
 
-    # can regret be negative?
-    for c_t, r_eat_t, r_no_eat_t, a_opt in zip(*mushrooms):
-        a_t = policy(c_t)
-        r_t = a_t * r_eat_t + (1 - a_t) * r_no_eat_t
-        r_t_opt =  a_opt * r_eat_t + (1 - a_opt) * r_no_eat_t
-        cum_rewards += r_t
+    for i, policy in enumerate(policies):
 
-        reg_t = r_t_opt - r_t
-        cum_regrets += reg_t
-        t += 1
+        rewards = np.zeros(T)
+        regrets = np.zeros(T)
+        t = 0
 
-    print("rewards")
-    print(cum_rewards/T)
+        # can regret be negative?
+        for c_t, r_eat_t, r_no_eat_t, a_opt in zip(*mushrooms):
+            a_t = policy.choose_action(c_t)
+            r_t = a_t * r_eat_t + (1 - a_t) * r_no_eat_t
 
-    print("regrets")
-    print(cum_regrets/T)
+            policy.update(a_t, c_t, r_t)
 
-def random_policy(c_t):
-    return np.random.choice([0,1])
+            r_t_opt =  a_opt * r_eat_t + (1 - a_opt) * r_no_eat_t
+
+            rewards[t] = r_t
+            regrets[t] = r_t_opt - r_t
+
+            t += 1
+
+        results[i] = (np.mean(rewards), np.mean(regrets))
+
+    print(results)
+
+
+
+
+class RandomPolicy(object):
+    def __init__(self, n_actions):
+        self._n_actions = n_actions
+
+    def choose_action(self, c_t):
+        return np.random.choice(np.arange(self._n_actions))
+
+    def update(self, a_t, c_t, r_t):
+        pass
+
+
+class SampleMeanPolicy(object):
+    def __init__(self, n_actions, lr=0.1):
+        self._Q = np.zeros(n_actions)
+        self._act_count = np.zeros(n_actions)
+        self._lr = lr
+
+    def choose_action(self, c_t):
+        a_t = np.argmax(self._Q)
+        self._act_count[a_t] += 1
+        return a_t
+
+    def update(self, a_t, c_t, r_t):
+        # ignores context
+        n_j = self._act_count[a_t]
+        self._Q[a_t] = 1/n_j * (self._Q[a_t] *(n_j - 1) + r_t)
+
+
+class EpsilonGreedyPolicy(object):
+    def __init__(self, n_actions, lr=0.1, epsilon=0.1):
+        self._n_actions = n_actions
+        self._Q = np.zeros(n_actions)
+        self._act_count = np.zeros(n_actions)
+        self._lr = lr
+        self._eps = epsilon
+
+    def choose_action(self, c_t):
+        u = np.random.uniform()
+        if u > self._eps:
+            a_t = np.argmax(self._Q)
+        else:
+            # choose random
+            a_t = np.random.choice(np.arange(self._n_actions))
+        self._act_count[a_t] += 1
+        return a_t
+
+    def update(self, a_t, c_t, r_t):
+        # ignores context
+        n_j = self._act_count[a_t]
+        self._Q[a_t] = 1/n_j * (self._Q[a_t] *(n_j - 1) + r_t)
+
+
+class UCBPolicy(object):
+    def __init__(self, n_actions, lr=0.1):
+        self._n_actions = n_actions
+        self._Q = np.zeros(n_actions)
+        self._act_count = np.zeros(n_actions, dtype=int)
+        self._lr = lr
+        self._t = 1
+
+    def choose_action(self, c_t):
+        ubc_t = np.zeros(self._n_actions)
+        for a_j in range(self._n_actions):
+            # compute upper bound
+            n_j = self._act_count[a_j]
+            if n_j == 0:
+                ubc_t[a_j] = np.inf
+            else:
+                ubc_t[a_j] = np.sqrt(2*np.log(self._t)/n_j)
+
+
+        if self._t % 10000 == 0:
+            print("ubc {} at {}".format(ubc_t, self._t))
+            print("act count", self._act_count)
+        a_t = np.argmax(self._Q + ubc_t)
+        self._act_count[a_t] += 1
+        self._t += 1
+        return a_t
+
+    def update(self, a_t, c_t, r_t):
+        # ignores context
+        n_j = self._act_count[a_t]
+        self._Q[a_t] = 1/n_j * (self._Q[a_t] *(n_j - 1) + r_t)
+        #self._Q[a_t] = self._Q[a_t] + self._lr * (r_t - self._Q[a_t])
+
+
+
 
 if __name__ == "__main__":
     args = []
