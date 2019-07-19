8a39e434ac1a78a1317fa0920b06754389419e64
dh <dhfromkorea@gmail.com>
2019-07-18 20:04:39 -0400

    add tests/context.py
    - to solve import errors for tests/

---
+++ tests/context.py
@@ -0,0 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from datasets.mushroom.sample_data import sample_mushroom
+from datasets.preprocessing import load_data
+
+
