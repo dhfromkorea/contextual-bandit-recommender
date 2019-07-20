8ca421135234f08e641e40f2c8bad1efec5604df
dh <dhfromkorea@gmail.com>
2019-07-20 19:53:57 -0400

    polish plotting.py
    - add things like legends

--- plotting.py
+++ plotting.py
@@ -5,11 +5,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_palette("husl")
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
 
 def main():
+    sns.set()
+    sns.set_palette("husl")
+
     tasks = ["mushroom", "synthetic"]
     for task in tasks:
         cumreg_path = os.path.join(root_dir, "{}.cumreg.csv".format(task))
@@ -21,25 +23,33 @@ def main():
         plot_cumreg(task, df_cumreg)
         plot_acts(task, df_acts)
 
+
 def plot_cumreg(task_name, df_cumreg):
-    plt.figure(figsize=(10, 10))
+    fig = plt.figure(figsize=(10, 10))
+    plt.xlabel("Rounds (t)", fontsize=20)
+    plt.ylabel("Cumulative Regret", fontsize=20)
     for j in range(df_cumreg.shape[1]):
         y = df_cumreg.iloc[:, j]
         plt.plot(np.arange(df_cumreg.shape[0]), y, label=df_cumreg.columns[j])
-    plt.legend(fontsize=20)
-    plt.savefig("{}.cumreg.png".format(task_name), bbox_inches="tight")
+    plt.legend(bbox_to_anchor=(1,0), loc="lower right",
+              bbox_transform=fig.transFigure, ncol= df_cumreg.shape[1])
+    plt.savefig("{}.cumreg.png".format(task_name))
     plt.close()
 
 
 def plot_acts(task_name, df_acts):
     fig = plt.figure(figsize=(10, 10))
+    plt.xlabel("Rounds (t)", fontsize=20)
+    plt.ylabel("Chosen action (a_t)", fontsize=20)
     for j in range(df_acts.shape[1]):
         y = df_acts.iloc[:, j]
-        plt.scatter(np.arange(df_acts.shape[0]), y + j * 0.05, label=df_acts.columns[j])
+        plt.scatter(np.arange(df_acts.shape[0]), y + j * 0.05,
+                label=df_acts.columns[j], s=3)
     plt.yticks(np.arange(df_acts.shape[1]))
     plt.legend(bbox_to_anchor=(1,0), loc="lower right",
-              bbox_transform=fig.transFigure, ncol= df_acts.shape[1], fontsize=20)
-    plt.savefig("{}.acts.png".format(task_name), bbox_inches="tight")
+              bbox_transform=fig.transFigure, ncol= df_acts.shape[1],
+              markerscale=6)
+    plt.savefig("{}.acts.png".format(task_name))
     plt.close()
 
 
