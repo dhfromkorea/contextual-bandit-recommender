6a847b84f5d194452c51a51e2582939311a8de6f
dh <dhfromkorea@gmail.com>
2019-07-28 01:04:42 -0400

    Fix action_log plot

--- plotting.py
+++ plotting.py
@@ -10,7 +10,10 @@ import seaborn as sns
 root_dir = os.path.abspath(os.path.dirname(__file__))
 
 def arg_parser():
+    TASK_LIST = ["mushroom", "synthetic", "news"]
+
     parser = argparse.ArgumentParser()
+    parser.add_argument("task", type=str, choices=TASK_LIST)
     parser.add_argument("--n_trials", type=int, default=1, help="number of \
             independent trials for experiments")
 
@@ -21,17 +24,15 @@ def arg_parser():
 
 def main():
     args = arg_parser()
-
     sns.set()
     sns.set_palette("husl")
 
-    tasks = ["news"]
-    for task in tasks:
-        plot_acb(task, args)
 
-    tasks = ["mushroom", "synthetic"]
-    for task in tasks:
-        plot_cb(task, args)
+    if args.task in ["news"]:
+        plot_acb(args.task, args)
+
+    if args.task in ["mushroom", "synthetic"]:
+        plot_cb(args.task, args)
 
 
 def compute_mean_std(paths, n_trials, window=-1):
@@ -104,8 +105,11 @@ def plot_cb(task, args):
     for i in range(n_trials):
         p = os.path.join(root_dir, "{}.acts.{}.csv".format(task, i))
         paths.append(p)
-    M_cr_mean, M_cr_std, columns = compute_mean_std(paths, n_trials)
-    plot_acts(task, M_cr_mean, columns)
+    #M_cr_mean, M_cr_std, columns = compute_mean_std(paths, n_trials)
+
+    # @todo: fix this
+    paths = [paths[0]]
+    plot_acts(task, paths, columns)
 
 
 def plot_cumreg(task_name, M_cr_mean, M_cr_std, columns):
@@ -119,26 +123,33 @@ def plot_cumreg(task_name, M_cr_mean, M_cr_std, columns):
     for j in range(M_cr_mean.shape[1]):
         y_mean = M_cr_mean[:, j]
         #y_std = M_cr_std[:, j]
-        ax.plot(x, y_mean, label=columns[j], linewidth=3)
+        ax.plot(x, y_mean + j * 0.01, label=columns[j], linewidth=1)
         #ax.fill_between(x, np.maximum(0, y_mean - y_std), y_mean + y_std,
         #                 alpha=0.3)
-        ax.fill_between(x, np.maximum(0, low[:,j]), high[:,j], alpha=0.2)
+        ax.fill_between(x, np.maximum(0, low[:,j]), high[:,j], alpha=0.1)
+
     ax.legend(loc="upper left", fontsize=15)
     fig.savefig("{}.cumreg.png".format(task_name), bbox_inches="tight")
     plt.close()
 
 
-def plot_acts(task_name, M_acts, columns):
+def plot_acts(task_name, paths, columns):
     plt.figure(figsize=(7, 7))
     plt.title("Action Log ({})".format(task_name), fontsize=25)
     plt.xlabel("Rounds (t)", fontsize=25)
     plt.ylabel("Chosen action (a_t)", fontsize=25)
-    for j in range(M_acts.shape[1]):
-        y = M_acts[:, j]
-        plt.scatter(np.arange(M_acts.shape[0]), y + j * 0.05,
-                label=columns[j], s=3)
-    plt.legend(bbox_to_anchor=(1.04,1), borderaxespad=0, markerscale=6,
-            fontsize=15)
+
+    for p in paths:
+        # for each trial data
+        df = pd.read_csv(p)
+        columns = df.columns
+        M_acts = df.to_numpy()
+        for j in range(M_acts.shape[1]):
+            y = M_acts[:, j]
+            plt.scatter(np.arange(M_acts.shape[0]), y + j * 0.05,
+                    label=columns[j], s=1)
+        plt.legend(bbox_to_anchor=(1.04,1), borderaxespad=0, markerscale=6,
+                fontsize=15)
     plt.savefig("{}.acts.png".format(task_name), bbox_inches="tight")
     plt.close()
 
@@ -155,10 +166,11 @@ def plot_cumrew(task_name, M_cr_mean, M_cr_std, columns):
         y_mean = M_cr_mean[:, j]
         #y_std = M_cr_std[:, j]
 
-        ax.plot(x, y_mean, label=columns[j], linewidth=3)
+        ax.plot(x, y_mean, label=columns[j], linewidth=1)
         #ax.fill_between(x, np.maximum(0, y_mean - y_std), y_mean + y_std,
         #                 alpha=0.3)
-        ax.fill_between(x, np.maximum(0, low[:, j]), high[:, j], alpha=0.2)
+        ax.fill_between(x, np.maximum(0, low[:, j]), high[:, j], alpha=0.1)
+
     ax.legend(loc="upper left", fontsize=15)
     fig.savefig("{}.cumrew.png".format(task_name), bbox_inches="tight")
     plt.close()
@@ -177,10 +189,10 @@ def plot_CTR(task_name, M_cr_mean, M_cr_std, columns, window):
         y_mean = M_cr_mean[:, j]
         #y_std = M_cr_std[:, j]
 
-        ax.plot(x, y_mean, label=columns[j], linewidth=3)
+        ax.plot(x, y_mean, label=columns[j])
         #ax.fill_between(x, np.maximum(0.0, y_mean - y_std), y_mean + y_std,
         #                 alpha=0.3)
-        ax.fill_between(x, np.maximum(0, low[:,j]), high[:,j], alpha=0.2)
+        ax.fill_between(x, np.maximum(0, low[:,j]), high[:,j], alpha=0.1)
 
     ax.legend(loc="upper left", fontsize=15)
     fig.savefig("{}.CTR.png".format(task_name), bbox_inches="tight")
